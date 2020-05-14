dc4de0c93522d79ec2a7d008427cdda6f00648d9
Eric Zocher <ezocher@users.noreply.github.com>
2020-05-13 20:51:09 -0700

    UWP Compiling and running. Circles are rendering but not animating.
    Also fixed background, text, and button colors.

---
+++ GravitySandboxUWP/GravitySandboxUWP/GravitySim.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+
+namespace GravitySandboxUWP
+{
+    // The simulation owns the bodies in the simulation space and holds references to their graphics incarnations (which are animated by the SimRender class)
+    class GravitySim
+    {
+        public enum simulationType { spaceSimulation, earthSimulation };
+        public enum bodyStartPosition
+        {
+            stageLeft, stageRight, stageTop, stageBottom, stageTopLeft, stageTopRight, stageBottomRight, stageBottomLeft,
+            screenTop, screenLeft, screenRight, screenBottom,
+            centerOfTheUniverse,
+            randomStagePosition, randomScreenPosition, randomCircularCluster
+        };
+
+        private simulationType simType;
+        public SimRender renderer;
+        private List<Flatbody> bodies;
+        private Canvas simCanvas;
+        private MainPage simPage;
+        private int monitoredBody = 0;
+        private double simElapsedTime;
+        private bool checkSim;
+        private int simRounding;
+        private Point[] accelerations;
+
+        public GravitySim(Canvas simulationCanvas, MainPage simulationPage)
+        {
+            simType = simulationType.spaceSimulation;
+            simCanvas = simulationCanvas;
+            simPage = simulationPage;
+            renderer = new SimRender(simType, simCanvas);
+            simElapsedTime = 0.0;
+            bodies = new List<Flatbody>();
+            checkSim = false;
+            simRounding = 0;
+        }
+
+        public void AddBody(double mass, double size, int color, bodyStartPosition startPosition)
+        {
+            bodies.Add(new Flatbody(mass, size, renderer.GetStartingPosition(startPosition)));
+            renderer.Add(size, color, bodies.Last());
+        }
+
+        public void AddBody(double mass, double size, int color, bodyStartPosition startPosition, Point startVelocity)
+        {
+            bodies.Add(new Flatbody(mass, size, renderer.GetStartingPosition(startPosition), startVelocity));
+            renderer.Add(size, color, bodies.Last());
+        }
+
+        public void SetCheckSim(bool check)
+        {
+            checkSim = check;
+        }
+
+        public void SetSimRounding(int roundingDigits)
+        {
+            simRounding = roundingDigits;
+        }
+
+        public void ClearSim()
+        {
+            bodies.Clear();
+            renderer.ClearSim();
+            simElapsedTime = 0.0;
+            checkSim = false;
+            simRounding = 0;
+            accelerations = null;
+        }
+
+        // The scale, origin, or layout changed so we need to re-transform all of the rendered bodies
+        public void TransformChanged()
+        {
+            renderer.TransformChanged(bodies);
+        }
+
+
+        // TBD: add + operator to Point class ??
+        public void Step(double timeInterval)
+        {
+            const double defaultAccelerationLimit = 10.0; // SimSpaceUnits per second^2
+
+            simElapsedTime += timeInterval;
+            if (accelerations == null)
+                accelerations = new Point[bodies.Count()];
+
+            // Calculate NBody acceleration
+            for (int i = 0; i < bodies.Count(); i++)
+            {
+                accelerations[i].X = 0.0;
+                accelerations[i].Y = 0.0;
+                for (int j = 0; j < bodies.Count(); j++)
+                    if (i != j)
+                    {
+                        Point a = bodies[i].BodyToBodyAccelerate(bodies[j]);
+                        accelerations[i].X += a.X;
+                        accelerations[i].Y += a.Y;
+                    }
+            }
+
+            // if (accelerationLimit)
+            EnforceAccelerationLimit(accelerations, defaultAccelerationLimit);
+
+            if (simRounding > 0)
+                RoundAccelerations(accelerations, simRounding);
+
+            // Update positons and velocity
+            for (int i = 0; i < bodies.Count(); i++)
+            {
+                bodies[i].Move(accelerations[i], timeInterval);
+                if (i == monitoredBody)
+                {
+                    simPage.UpdateMonitoredValues(bodies[i], simElapsedTime);
+                }
+            }
+
+            if (checkSim)
+                ValidateState(accelerations);
+
+            // Update rendering
+            renderer.BodiesMoved(bodies);
+        }
+
+        public void EnforceAccelerationLimit(Point[] accelerations, double limit)
+        {
+            for (int i = 0; i < accelerations.Length; i++)
+            {
+                double m = Magnitude(accelerations[i]);
+                if (m > limit)
+                {
+                    double scale = limit / m;
+                    accelerations[i].X = accelerations[i].X * scale;
+                    accelerations[i].Y = accelerations[i].Y * scale;
+                }
+            }
+        }
+
+
+        private void ValidateState(Point[] accelerations)
+        {
+            bool invalid;
+
+            invalid = false;
+            if (!CheckMirrorAndZeroY(accelerations[0], accelerations[2]))
+                invalid = true;
+            if (!CheckMirrorAndZeroY(bodies[0].Position, bodies[2].Position))
+                invalid = true;
+            if (!CheckMirrorAndZeroY(bodies[0].Velocity, bodies[2].Velocity))
+                invalid = true;
+            if (!CheckMirrorAndZeroX(accelerations[1], accelerations[3]))
+                invalid = true;
+            if (!CheckMirrorAndZeroX(bodies[1].Position, bodies[3].Position))
+                invalid = true;
+            if (!CheckMirrorAndZeroX(bodies[1].Velocity, bodies[3].Velocity))
+                invalid = true;
+            if (!CrossCheckXY(accelerations[0], accelerations[3]))
+                invalid = true;
+            if (!CrossCheckXY(bodies[0].Position, bodies[3].Position))
+                invalid = true;
+            if (!CrossCheckXY(bodies[0].Velocity, bodies[3].Velocity))
+                invalid = true;
+            if (!CrossCheckXY(accelerations[1], accelerations[2]))
+                invalid = true;
+            if (!CrossCheckXY(bodies[1].Position, bodies[2].Position))
+                invalid = true;
+            if (!CrossCheckXY(bodies[1].Velocity, bodies[2].Velocity))
+                invalid = true;
+
+            if (invalid)
+                invalid = true;
+        }
+
+        private static bool CheckMirrorAndZeroY(Point a, Point b)
+        {
+            if ((a.Y != 0.0) || (b.Y != 0.0) || (a.X != -b.X))
+                return false;
+            else
+                return true;
+        }
+
+        private static bool CheckMirrorAndZeroX(Point a, Point b)
+        {
+            if ((a.X != 0.0) || (b.X != 0.0) || (a.Y != -b.Y))
+                return false;
+            else
+                return true;
+        }
+
+        private static bool CrossCheckXY(Point a, Point b)
+        {
+            if ((a.X != b.Y) || (a.Y != b.X))
+                return false;
+            else
+                return true;
+        }
+
+
+        private void RoundAccelerations(Point[] accelerations, int roundingDigits)
+        {
+            for (int i = 0; i < accelerations.Length; i++)
+            {
+                accelerations[i].X = Math.Round(accelerations[i].X, roundingDigits);
+                accelerations[i].Y = Math.Round(accelerations[i].Y, roundingDigits);
+            }
+        }
+
+        // const double epsilon = 0.001;  // Horizontal, vertical or 45 degree diagonal acceleration will start to drift because of cumulative
+        //  rounding error. For differences less than epsilon, snap the values back to zero or the 
+        //  perfect diagonal.
+
+
+        /*// Clean X & Y to eliminate cumulative rounding errors
+double absX = Math.Abs(accelerations[i].X);
+double absY = Math.Abs(accelerations[i].Y);
+
+if (Math.Abs(absX - absY) < epsilon)
+{
+    double avgDiagonalAccel = (absX + absY) / 2.0;
+    accelerations[i].X = Math.Sign(accelerations[i].X) * avgDiagonalAccel;
+    accelerations[i].Y = Math.Sign(accelerations[i].Y) * avgDiagonalAccel;
+}
+else
+{
+                
+    if ((absX > 0.0) && (absX < epsilon))
+        accelerations[i].X = 0.0;
+    if ((absY > 0.0) && (absY < epsilon))
+        accelerations[i].Y = 0.0;
+**/
+
+        // Treats a point as a vector and calculates its magnitude
+        public static double Magnitude(Point v)
+        {
+            return (Math.Sqrt((v.X * v.X) + (v.Y * v.Y)));
+        }
+
+        public void SetMonitoredValues()
+        {
+            simPage.UpdateMonitoredValues(bodies[monitoredBody], simElapsedTime);
+        }
+
+        public void SetMonitoredBody(int monitorBody)
+        {
+            monitoredBody = monitorBody;
+            renderer.SetMonitoredColor(monitoredBody);
+        }
+
+    }
+}
---
+++ GravitySandboxUWP/GravitySandboxUWP/SimRender.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+namespace GravitySandboxUWP
+{
+    class SimRender
+    {
+        public enum colorNumber { bodyColorRed = 1, bodyColorGreen, bodyColorBlue, bodyColorLtGrey = 9, bodyColorMedGrey, bodyColorDarkGrey };
+
+        // The simulation box is a square with it's own coordinate space
+        //      For simulations in space the origin is in the center of the square
+        //      For simulations on earth, the earth surface is a flat strip across the bottom of the screen and the origin is horizontally centered with y = 0 at ground level
+        //
+        // The simulation box will be centered on the display and will be the maximum size it can be while remaining completely on the screen
+        //
+        const double simBoxHeightAndWidth = 1000.0;
+        const double simBoxMaxXY = simBoxHeightAndWidth / 2.0;
+
+        const double earthStripHeight = 20.0;
+
+        const double circleSizeFactor = 10.0;  // the smallest body will have a simulaton size of 1.0, render it with this diameter in simulation space
+
+
+        private Point screenDimensions;     // screen dimensions in rendering space
+        private double scaleFactor;         // use scaleFactor for x, use -scaleFactor for y - incorporates any zoom factor
+        private double zoomFactor;          // zoom factor, 1.0 = 100%
+        private Point simulationCenterTranslation;
+        private Point screenSimulationDimensions;    // screen width and height in simulation space
+
+        private Canvas simCanvas;
+
+        private Random rand;
+
+        // Mapping point in simulation space to rendering space:
+        //  simPt * scaleFactor + simulationCenterTranslation + circleCenterTranslation -> renderingOffset
+
+
+        private GravitySim.simulationType simType;
+
+        private List<Ellipse> circles;
+
+        const string circleColorResourceBaseString = "bodyColor";
+        const string circleColorMonitoredColorResourceName = "monitoredBodyColor";
+
+        public const int firstColorIndex = 1;
+        public const int lastColorIndex = 10;
+
+        public SimRender(GravitySim.simulationType type, Canvas simulationCanvas)
+        {
+            this.simType = type;
+            this.circles = new List<Ellipse>();
+            this.rand = new Random();
+            this.simCanvas = simulationCanvas;
+            this.zoomFactor = 1.0;
+        }
+
+        public void Add(double size, int colorNumber, Flatbody body)
+        {
+            Ellipse newCircle = new Ellipse();
+            newCircle.Width = newCircle.Height = CircleSize(size);
+            newCircle.Fill = (SolidColorBrush)Application.Current.Resources[circleColorResourceBaseString + colorNumber.ToString()];
+            newCircle.RenderTransform = CircleTransform(body);
+            simCanvas.Children.Add(newCircle);
+            circles.Add(newCircle);
+        }
+
+        public void SetMonitoredColor(int monitoredCircleIndex)
+        {
+            circles[monitoredCircleIndex].Fill = (SolidColorBrush)Application.Current.Resources[circleColorMonitoredColorResourceName];
+        }
+
+        public void ClearSim()
+        {
+            simCanvas.Children.Clear();
+            circles.Clear();
+            scaleFactor = scaleFactor / zoomFactor;
+            zoomFactor = 1.0;
+        }
+
+        public void TransformChanged(List<Flatbody> bodies)
+        {
+            int i = 0;
+            Ellipse circle;
+            foreach (Flatbody body in bodies)
+            {
+                circle = circles[i++];
+                circle.Width = circle.Height = CircleSize(body.Size);
+                /* if (CircleOnscreen(circle))
+                    circle.Visibility = Visibility.Visible;
+                else
+                    circle.Visibility = Visibility.Collapsed; */
+                circle.RenderTransform = CircleTransform(body);
+            }
+        }
+
+        /* Original BodiesMoved()
+                public void BodiesMoved(List<Flatbody> bodies)
+                {
+                    int i = 0;
+                    Ellipse circle;
+                    foreach (Flatbody body in bodies)
+                    {
+                        circle = circles[i++];
+                        circle.RenderTransform = CircleTransform(body);
+
+                        if (CircleOnscreen(circle))
+                            circle.Visibility = Visibility.Visible;
+                        else
+                            circle.Visibility = Visibility.Collapsed;
+                    }
+                }
+        */
+
+        public void BodiesMoved(List<Flatbody> bodies)
+        {
+            for (int i = 0; i < bodies.Count; i++)
+            {
+                circles[i].RenderTransform = CircleTransform(bodies[i]);
+
+                /* if (CircleOnscreen(circles[i]))
+                    circles[i].Visibility = Visibility.Visible;
+                else
+                    circles[i].Visibility = Visibility.Collapsed; */
+            }
+        }
+
+        // TBD: test if the bounding rectangle of the circle intersects the screen rectangle
+        public bool CircleOnscreen(Ellipse circle)
+        {
+            return true;
+        }
+
+
+        // The simulation space is a 
+        public void SetSimulationTransform(double screenWidth, double screenHeight)
+        {
+            screenDimensions = new Point(screenWidth, screenHeight);
+            double minDimension = Math.Min(screenWidth, screenHeight);
+
+            if (simType == GravitySim.simulationType.spaceSimulation)
+            {
+                scaleFactor = minDimension / simBoxHeightAndWidth;
+                simulationCenterTranslation = new Point(screenWidth / 2, screenHeight / 2);
+                screenSimulationDimensions = new Point(screenWidth / scaleFactor, screenHeight / scaleFactor);
+            }
+            else // unimplemented simType
+            {
+                throw new ArgumentException("Unimplemented simulation type:" + simType.ToString());
+            }
+            scaleFactor = scaleFactor * zoomFactor;
+        }
+
+
+        private double CircleSize(double bodySize)
+        {
+            return (bodySize * circleSizeFactor * scaleFactor);
+        }
+
+
+        // Mapping point in simulation space to rendering space:
+        //  simPt * scaleFactor + simulationCenterTranslation + circleCenterTranslation -> renderingOffset
+        public TranslateTransform CircleTransform(Flatbody body)
+        {
+            TranslateTransform t = new TranslateTransform();
+            double circleCenterTranslation = -CircleSize(body.Size) / 2.0;
+
+            t.X = body.Position.X * scaleFactor + simulationCenterTranslation.X + circleCenterTranslation;
+            t.Y = body.Position.Y * -scaleFactor + simulationCenterTranslation.Y + circleCenterTranslation;
+
+            return t;
+        }
+
+
+        // Returns starting position for a body in simulation space coordinates
+        public Point GetStartingPosition(GravitySim.bodyStartPosition startPos)
+        {
+            const double stagePosition = 0.5; // For the "stage" positions - proportion of the way from the center of the stage to the edge
+                                              //    in all directions
+            double stageXY = simBoxMaxXY * stagePosition;
+
+            double screenMaxX = screenSimulationDimensions.X / 2.0;
+            double screenMaxY = screenSimulationDimensions.Y / 2.0;
+
+            switch (startPos)
+            {
+                case GravitySim.bodyStartPosition.stageLeft:
+                    return new Point(-stageXY, 0.0);
+                case GravitySim.bodyStartPosition.stageRight:
+                    return new Point(stageXY, 0.0);
+                case GravitySim.bodyStartPosition.stageTop:
+                    return new Point(0.0, stageXY);
+                case GravitySim.bodyStartPosition.stageBottom:
+                    return new Point(0.0, -stageXY);
+
+                case GravitySim.bodyStartPosition.stageTopLeft:
+                    return new Point(-stageXY, stageXY);
+                case GravitySim.bodyStartPosition.stageTopRight:
+                    return new Point(stageXY, stageXY);
+                case GravitySim.bodyStartPosition.stageBottomLeft:
+                    return new Point(-stageXY, -stageXY);
+                case GravitySim.bodyStartPosition.stageBottomRight:
+                    return new Point(stageXY, -stageXY);
+
+                case GravitySim.bodyStartPosition.screenLeft:
+                    return new Point(-screenMaxX, 0.0);
+                case GravitySim.bodyStartPosition.screenRight:
+                    return new Point(screenMaxX, 0.0);
+                case GravitySim.bodyStartPosition.screenTop:
+                    return new Point(0.0, screenMaxY);
+                case GravitySim.bodyStartPosition.screenBottom:
+                    return new Point(0.0, -screenMaxY);
+
+                case GravitySim.bodyStartPosition.centerOfTheUniverse:
+                    return new Point(0.0, 0.0);
+
+                case GravitySim.bodyStartPosition.randomStagePosition:
+                    return new Point(rand.Next((int)-simBoxMaxXY, (int)simBoxMaxXY),
+                                      rand.Next((int)-simBoxMaxXY, (int)simBoxMaxXY));
+                case GravitySim.bodyStartPosition.randomScreenPosition:
+                    return new Point(rand.Next((int)-screenMaxX, (int)screenMaxX),
+                                      rand.Next((int)-screenMaxY, (int)screenMaxY));
+                case GravitySim.bodyStartPosition.randomCircularCluster:
+                    double length = rand.NextDouble() * simBoxMaxXY * 0.75;
+                    double angle = rand.NextDouble() * Math.PI * 2.0; // radians
+                    return new Point(length * Math.Cos(angle), length * Math.Sin(angle));
+                // TBD: Add random circular cluster with uniform density
+
+
+                default:
+                    return new Point(0.0, 0.0);
+            }
+        }
+    }
+}
