4270cd91b430166fe5a425489c589ffb972e4794
Eric Zocher <ezocher@users.noreply.github.com>
2020-05-18 23:13:21 -0700

    Start support for multiple coordinate/time/units systems
    Create SimulationSpaces.cs.

---
+++ GravitySandboxUWP/GravitySandboxUWP/SimulationSpaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravitySandboxUWP
+{
+    /* Definitions of dimensions and units in space, mass, and time for simulation spaces
+     * 
+     * Screen space is always in UWP pixel units and seconds
+     * 
+     */
+    class SimulationSpaces
+    {
+
+
+        // Original simulation space: toySpace
+        // Dimensions   Mass   Time    Velocity
+
+    }
+}
